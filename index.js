67c9be9cf4a617cbd2497fd19de5b53661070f6d
unknown <memorytemp5@gmail.com>
2024-04-01 21:05:59 +0530

    add get data route for test '

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
+app.get('/get-data', (req, res) => {
+    res.status(200).json({
+        "data": "Get Data"
+    });
+});
 app.listen(port, () => {
     console.log(`Server running at ${port}`);
 })
